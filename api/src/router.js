549ca89e241c1c97b2d55c44e9aa31a5781fad96
Sven Piller <sven.piller@gmail.de>
2020-11-21 23:24:22 +0100

    Image Service Worker

--- api/src/router.js
+++ api/src/router.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const path = require("path");
 const multer = require("multer");
 const {
     response
@@ -13,6 +14,7 @@ const upload = multer({
     fileFilter: fileFilter,
     storage: storage
 });
+const photoPath = path.resolve(__dirname, '../../client/photo-viewer.html')
 
 function filename(request, file, callback) {
     callback(null, file.originalname)
@@ -37,6 +39,10 @@ router.post('/upload', upload.single('photo'), (req, res) => {
             success: true
         })
     }
+})
+
+router.get('/photo-viewer', (req,res)=> {
+    res.sendFile(photoPath);
 })
 
 
